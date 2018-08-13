6b1a8e7fbec09251421529b20a3339ac65748ce9
mattj241 <londonm241@gmail.com>
2018-08-13 12:20:02 -0400

    Completely changed insert and removal methods
    I found with the previous methods, for whatever reason, the threads would get stuck and not look very impressive from a grading standpoint.
    
    Comments in the code will make this more clear.

--- 450final/450final/Main_ConsumersAndProducers.cpp
+++ 450final/450final/Main_ConsumersAndProducers.cpp
@@ -17,7 +17,6 @@ The libraries pthread.h and semaphore.h from POSIX were used.
 using namespace std;
 
 #define NUM_BUFFERS 7 //From assignment specification
-//int buffer[NUM_BUFFERS] = { -1, -1, -1, -1, -1, -1, -1 };
 int buffer[NUM_BUFFERS] = { 0, 0, 0, 0, 0, 0, 0 };
 
 
@@ -30,38 +29,57 @@ random_device rd;
 mt19937 gen(rd());
 
 /*
-Description: Inserts the parameter value into the main buffer.
-Pre: Called in the Produce function by a pthread.
+Description: Inserts the parameter value into the main buffer. Starts at the bottom of the buffer to fill any holes.
+			 If no holes, it fills the back of the buffer. If the back of the buffer is full, then the insert is abandoned.
+Pre: Called in the Produce function by a pthread. The physical value to be inserted and the thread ID number is passed in.
 Post: If the buffer was not full, the value is inserted to the main buffer.
 */
-void insertIntoBuffer(int value)
+void insertIntoBuffer(int value, int threadNum)
 {
-	if (sizeTracker < NUM_BUFFERS)
+	
+	bool openSpotFound = false;
+	int counter = 0;
+
+	while (!openSpotFound && counter < NUM_BUFFERS)
 	{
-		buffer[sizeTracker] = value;
-		sizeTracker = sizeTracker + 1;
+		if (buffer[counter] < 1)
+		{
+			openSpotFound = true;
+			buffer[counter] = value;
+			cout << "Produced {" << value << "} into index ["
+				<< counter << "] with Producer thread "
+				<< threadNum << endl;
+		}
+		counter++;
 	}
-	else
+	if (counter == NUM_BUFFERS)
 	{
-		cout << "buffer full" << endl;
-	}
+		cout << "buffer is full" << endl;
+	}	
 }
 
 /*
-Description: Removes the value in the parameter index from main buffer.
-Pre: Called in the Consume function by a pthread.
+Description: Removes the value in the parameter index from main buffer. 
+			 It removes the value in the randomly selected index. If that index is empty, the removal is abandoned.
+Pre: Called in the Consume function by a pthread. The randomly selected index and the thread ID number is passed in.
 Post: If the buffer was not empty, the value is removed in the main buffer.
 */
-void removeFromBuffer(int index)
+void removeFromBuffer(int index, int threadNum)
 {
-	if (sizeTracker > 0)
+	int temp = buffer[index];
+	if (temp < 1)
 	{
-		buffer[index] = -1;
-		sizeTracker = sizeTracker - 1;
+		cout << "Nothing to Consume in randomly "
+			<< "selected index ["
+			<< index << "] with thread "
+			<< (int)threadNum << endl;
 	}
 	else
 	{
-		cout << "buffer empty" << endl;
+		cout << "Consumed {" << temp << "} from index ["
+			<< index << "] with Consumer thread "
+			<< threadNum << endl;
+		buffer[index] = -1;
 	}
 }
 
@@ -74,19 +92,16 @@ void * Produce(void * thread)
 {	
 	//generate a random number 1 - 100 for the buffer
 	uniform_int_distribution<> dis(1, 100);
+	int item;
 
 	while (true)
 	{
-		int item = dis(gen);
+		item = dis(gen);
 
 		sem_wait(&empty);				//decrement empty sempahore 
 		pthread_mutex_lock(&mutex);		//acquire lock
-		insertIntoBuffer(item);			//function call
-		cout << "Produced {" << item << "} into index ["
-			 << sizeTracker - 1 << "] with Producer thread " 
-			 << (int)thread << endl;
+		insertIntoBuffer(item, (int)thread);			//function call
 		sem_post(&full);				//increment full semaphore
-		Sleep(5);
 		pthread_mutex_unlock(&mutex);   //release lock
 	} 
 }
@@ -98,17 +113,17 @@ Post: Endlessy consumes items in the buffer.
 */
 void * Consume(void * thread)
 {
+	uniform_int_distribution<> dis(0, NUM_BUFFERS - 1);
+	int temp, randomIndex;
+
 	while (true)
-	{
+	{	
+		randomIndex = dis(gen);
 
 		sem_wait(&full);						//decrement full sempahore 
 		pthread_mutex_lock(&mutex);				//acquire lock
-		cout << "Consumed {" << buffer[sizeTracker - 1] << "} from index ["
-			<< sizeTracker - 1 << "] with Consumer thread "
-			 << (int)thread << endl;
-		removeFromBuffer(sizeTracker - 1);	//function call
+		removeFromBuffer(randomIndex, (int)thread);	//function call
 		sem_post(&empty);						//increment empty semaphore
-		Sleep(5);
 		pthread_mutex_unlock(&mutex);			//release lock
 	}
 }
